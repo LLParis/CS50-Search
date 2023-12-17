2e124b12b21e863c0204806bdd6db8f9dc29810e
Your Name <you@example.com>
2023-12-16 21:31:55 -0800

    Work In Progress

--- maze.py
+++ maze.py
@@ -38,10 +38,21 @@ class QueueFrontier(StackFrontier):
             return node
 
 class Maze():
-    def __init__(self):
+    def __init__(self, filename):
 
         # Read file and set height and width of maze
+        with open(filename) as f:
+            contents = f.read()
 
-    with open(filename) as f:
+        # Validate start and goal
+        if contents.count("A") != 1:
+            raise Exception("maze must have exactly one start point")
+        if contents.count("B") != 1:
+            raise Exception("maze must have exactly one end point")
+
+        # Determine height and width of maze
+        contents = contents.splitlines()
+        self.height = len(contents)
+        self.width = max(len(line) for line in contents)
 
 
